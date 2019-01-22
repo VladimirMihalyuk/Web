9be07bfcf6ba1be30999a415cd4d700622f757ab
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-01-22 23:16:37 +0300

    added all labs

---
+++ lab1/src/model/QuadrilateralType.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * This is enum of Quadrilateral geometric types
+ *
+ * @author Ilya Sysoi
+ * @version 1.0.0
+ */
+public enum QuadrilateralType {
+    SQUARE,
+    RECTANGLE,
+    RHOMBUS,
+    ARBITRARY
+}
