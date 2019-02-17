ba4d9e713e868e3cce2378da47546743ffee9901
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-02-18 02:36:57 +0300

    lab8: added demo and helper

--- lab8/src/by/isysoi/Controller/HorseRaceController.java
+++ lab8/src/by/isysoi/Controller/HorseRaceController.java
@@ -1,34 +1,57 @@
 package by.isysoi.Controller;
 
+import by.isysoi.Model.DAO.BetDAO;
 import by.isysoi.Model.DAO.RaceDAO;
+import by.isysoi.Model.Entity.Bet;
 import by.isysoi.Model.Entity.Client;
 import by.isysoi.Model.Entity.Horse;
 import by.isysoi.Model.Entity.Race;
+import by.isysoi.Model.Exception.DAOException;
 
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * controller
+ *
  * @author Ilya Sysoi
  * @version 1.0.0
  */
 public class HorseRaceController {
 
-    List<Horse> getHorsesByRaceId(int raceId) throws HorseRaceControllerException {
-
-        return  new LinkedList<>();
+    public List<Horse> getHorsesByRaceId(int raceId) throws HorseRaceControllerException {
+        List<Horse> horses;
+        try {
+            RaceDAO tmp = new RaceDAO();
+            horses = tmp.readHorcesInRace(raceId);
+        } catch (DAOException e) {
+            throw new HorseRaceControllerException("Failed to get horses by race id", e);
+        }
+        return horses;
     }
 
-    List<Race> getRacesByDate(Date date) throws HorseRaceControllerException {
-
-        return  new LinkedList<>();
+    public List<Race> getRacesByDate(Date date) throws HorseRaceControllerException {
+        List<Race> races;
+        try {
+            RaceDAO tmp = new RaceDAO();
+            races = tmp.readRacesByDate(date);
+        } catch (DAOException e) {
+            throw new HorseRaceControllerException("Failed to get races by date", e);
+        }
+        return races;
     }
 
-    List<Client> getWinnersByRaceId(int raceId) throws HorseRaceControllerException {
-
-        return  new LinkedList<>();
+    public List<Map.Entry<Client, Bet>> getWinnersByRaceId(int raceId) throws HorseRaceControllerException {
+        List<Map.Entry<Client, Bet>> clientsAndBets;
+        try {
+            BetDAO tmp = new BetDAO();
+            clientsAndBets = tmp.readWinnersByRace(raceId);
+        } catch (DAOException e) {
+            throw new HorseRaceControllerException("Failed to get winners", e);
+        }
+        return clientsAndBets;
     }
 
 }
--- lab8/src/by/isysoi/View/Main.java
+++ lab8/src/by/isysoi/View/Main.java
@@ -1,19 +1,31 @@
 package by.isysoi.View;
 
-import by.isysoi.Model.DAO.ClientDAO;
-import by.isysoi.Model.Entity.Client;
-import by.isysoi.Model.Exception.DAOException;
+import by.isysoi.Controller.HorseRaceController;
+import by.isysoi.Controller.HorseRaceControllerException;
+import by.isysoi.Utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
     public static void main(String[] args) {
+        var controller = new HorseRaceController();
         try {
-            ClientDAO tmp = new ClientDAO();
-            tmp.insertClient(new Client(4,"test1233"));
-        } catch (DAOException e) {
-            e.printStackTrace();
+            var horses = controller.getHorsesByRaceId(1);
+            Utils.printList(horses);
 
+            SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
+            var races = controller.getRacesByDate(ft.parse("11-02-2019"));
+            Utils.printList(races);
+
+            var winners = controller.getWinnersByRaceId(2);
+            Utils.printListOfTuples(winners);
+
+        } catch (HorseRaceControllerException | ParseException e) {
+            e.printStackTrace();
         }
 
     }
+
 }
