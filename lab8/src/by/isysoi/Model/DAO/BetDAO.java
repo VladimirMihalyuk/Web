ad5664648c4e27d849a3eacf737fe984520b5f7f
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-02-18 02:20:57 +0300

    lab8: get winners

--- lab8/src/by/isysoi/Model/DAO/BetDAO.java
+++ lab8/src/by/isysoi/Model/DAO/BetDAO.java
@@ -1,6 +1,7 @@
 package by.isysoi.Model.DAO;
 
 import by.isysoi.Model.Entity.Bet;
+import by.isysoi.Model.Entity.Client;
 import by.isysoi.Model.Exception.DAOException;
 import by.isysoi.Model.Exception.DBConnectionException;
 
@@ -9,11 +10,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * bet dao class
+ *
  * @author Ilya Sysoi
  * @version 1.0.0
  */
@@ -27,8 +31,11 @@ public class BetDAO extends DAO {
 
     private static final String SELECT_BET_BY_ID_SQL = "select * from bet where id = ?";
 
+    private static final String SELECT_WINNERS_BY_RACE_SQL = "select c.id, c.fio, b.id, b.amount from client c join bet b on b.client_id = c.id join race r on b.race_id = r.id join race_info ri on ri.race_id = r.id where b.race_id = ? and ri.position = 1";
+
     /**
      * constructor
+     *
      * @throws DAOException if Can't create connection
      */
     public BetDAO() throws DAOException {
@@ -37,8 +44,9 @@ public class BetDAO extends DAO {
 
     /**
      * read bets
-     * @throws DAOException if Can't execute query or problems with connection
+     *
      * @return bets
+     * @throws DAOException if Can't execute query or problems with connection
      */
     public List<Bet> readBet() throws DAOException {
         List<Bet> bets = new ArrayList<Bet>();
@@ -56,7 +64,7 @@ public class BetDAO extends DAO {
         } catch (SQLException e) {
             throw new DAOException("Delete Bet exception ", e);
         } catch (DBConnectionException e) {
-             throw new DAOException("Failed establish connection", e);
+            throw new DAOException("Failed establish connection", e);
         } finally {
             try {
                 getDBConnector().close();
@@ -69,8 +77,9 @@ public class BetDAO extends DAO {
 
     /**
      * read bet by id
-     * @throws DAOException if Can't execute query or problems with connection
+     *
      * @return bet
+     * @throws DAOException if Can't execute query or problems with connection
      */
     public Bet readBetById(int id) throws DAOException {
         Bet Bet = null;
@@ -87,7 +96,7 @@ public class BetDAO extends DAO {
         } catch (SQLException e) {
             throw new DAOException("Delete Bet exception ", e);
         } catch (DBConnectionException e) {
-             throw new DAOException("Failed establish connection", e);
+            throw new DAOException("Failed establish connection", e);
         } finally {
             try {
                 getDBConnector().close();
@@ -100,6 +109,7 @@ public class BetDAO extends DAO {
 
     /**
      * insety clients
+     *
      * @throws DAOException if Can't execute query or problems with connection
      */
     public void insertBet(Bet bet) throws DAOException {
@@ -112,7 +122,7 @@ public class BetDAO extends DAO {
         } catch (SQLException e) {
             throw new DAOException("Insert Bet exception ", e);
         } catch (DBConnectionException e) {
-             throw new DAOException("Failed establish connection", e);
+            throw new DAOException("Failed establish connection", e);
         } finally {
             try {
                 getDBConnector().close();
@@ -125,8 +135,8 @@ public class BetDAO extends DAO {
 
     /**
      * delete clients
+     *
      * @throws DAOException if Can't execute query or problems with connection
-     * @return list of clients
      */
     public void deleteClient(Bet bet) throws DAOException {
         try {
@@ -138,7 +148,7 @@ public class BetDAO extends DAO {
         } catch (SQLException e) {
             throw new DAOException("Delete Bet exception ", e);
         } catch (DBConnectionException e) {
-             throw new DAOException("Failed establish connection", e);
+            throw new DAOException("Failed establish connection", e);
         } finally {
             try {
                 getDBConnector().close();
@@ -148,5 +158,45 @@ public class BetDAO extends DAO {
         }
 
     }
-    
+
+    /**
+     * get winners by race
+     *
+     * @return list of clients
+     * @throws DAOException if Can't execute query or problems with connection
+     */
+    public List<Map.Entry<Client,Bet>> readWinnersByRace(int raceId) throws DAOException {
+        List<Map.Entry<Client,Bet>> clientsWithBet = new ArrayList<Map.Entry<Client,Bet>>();
+        try {
+            Connection connection = getDBConnector().getConnection();
+
+            PreparedStatement stmt = connection.prepareStatement(SELECT_WINNERS_BY_RACE_SQL);
+            stmt.setInt(1,raceId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int clientId = rs.getInt(1);
+                String fio = rs.getString(2);
+                Client client = new Client(clientId, fio);
+
+                int betId = rs.getInt(1);
+                BigDecimal amount = rs.getBigDecimal(2);
+                Bet bet = new Bet(betId,amount);
+
+                clientsWithBet.add(new AbstractMap.SimpleImmutableEntry<>(client, bet));
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Delete Bet exception ", e);
+        } catch (DBConnectionException e) {
+            throw new DAOException("Failed establish connection", e);
+        } finally {
+            try {
+                getDBConnector().close();
+            } catch (DBConnectionException e) {
+                throw new DAOException("Failed close connection", e);
+            }
+        }
+        return clientsWithBet;
+    }
+
+
 }
